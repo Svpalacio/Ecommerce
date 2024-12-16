22128fc3dc6bbd199af19b43e088e753ba666fe1
svpalacio <svpalacio74@gmail.com>
2024-12-15 22:03:45 -0300

    pre entrega 2- Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import NavBar from './components/NavBar/NavBar'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import NavBar from "./components/NavBar/NavBar"
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.scss'
+
 function App() {
 
   return (
-    <div>
-      <NavBar />
-      <ItemListContainer saludo={"Bienvenidos a mi ecommerce!!!"} />
+    <div className="container-app" >
+      <BrowserRouter>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={<ItemListContainer saludo={"Bienvenidos a mi ecommerce!"} />} />
+          <Route path="/category/:idCategory" element={<ItemListContainer saludo={"Bienvenidos a mi ecommerce!"} />} />
+          <Route path="/detail/:idProduct" element={<ItemDetailContainer />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
